e84943825270df8d7c27ebe8e9de335a645173a1
Samuel DeWilde Muthiah <42986926+samuthiah@users.noreply.github.com>
2024-07-29 23:19:25 -0500

    feat: clear button clears display

--- scripts.js
+++ scripts.js
@@ -18,6 +18,7 @@ let numFirst;
 let operator;
 let numSecond;
 
+// performs the 4 basic arithmetic operations based on the numbers and operator passed
 function operate(x, y, operator) {
     if (operator === '*') {
         return multiply(x, y);
@@ -33,15 +34,26 @@ function operate(x, y, operator) {
     } 
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
+    // selects the display of the calculator and declares a variable to store future display text
     const display = document.querySelector('.calcDisplay');
     let displayText;
+
+    // gets all number and operator buttons, then sets an event listener to see if they're clicked
     const calcButtons = document.querySelectorAll('.calcButton');
     calcButtons.forEach((button) => {
         button.addEventListener('click', () => {
+            // when a button is clicked, its text is displayed on the display and stored in a variable
             display.textContent += button.textContent;
             displayText = display.textContent;
-            console.log(displayText);
         })
+    })
+
+    // clear button clears dislpay
+    const clear = document.querySelector('.calcClearButton');
+    clear.addEventListener('click', () => {
+        display.textContent = '';
+        displayText = '';
     })
 })
