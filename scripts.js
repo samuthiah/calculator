cc89fc3680161d6621358033598c3d40f2a1cbce
Samuel DeWilde Muthiah <42986926+samuthiah@users.noreply.github.com>
2024-07-29 20:09:46 -0500

    feat: create operate function
    operate function takes two numbers and an operator and calls the appropriate arithmetic function with the two numbers as parameters

--- scripts.js
+++ scripts.js
@@ -13,3 +13,22 @@ function multiply(x, y) {
 function divide(x,y) {
     return x / y;
 }
+
+let numFirst;
+let operator;
+let numSecond;
+
+function operate(x, y, operator) {
+    if (operator === '*') {
+        return multiply(x, y);
+    } 
+    if (operator === '+') {
+        return add(x, y);
+    } 
+    if (operator === '-') {
+        return subtract(x, y);
+    } 
+    if (operator === '/') {
+        return divide(x, y);
+    } 
+}
